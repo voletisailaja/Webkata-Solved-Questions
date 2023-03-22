5cbe607b379c4a8d6f001a56c5b3d3686af6db3a
Voleti Sailaja <81481090+voletisailaja@users.noreply.github.com>
2023-03-22 15:24:39 +0530

    Create Q68.js

---
+++ Q68.js
@@ -0,0 +1,12 @@
+<style>
+    
+    p {
+        background-color:lightblue;
+        padding: 10px 10px 10px 10px;
+    }
+    </style>
+  </head>
+  <body>
+      <p>Hello from guvian</p>
+
+  </body>
